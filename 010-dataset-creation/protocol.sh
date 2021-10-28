d366a26be18f887e4a2d25876fba98919e64e0cc
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2021-10-28 22:59:03 +0200

    Server demo

--- 010-dataset-creation/protocol.sh
+++ 010-dataset-creation/protocol.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -x
-dtool create 20211027-demo-dataset
+dtool create demo-dataset
 
 echo test-content > test-data
 
-dtool add item test-data 20211027-demo-dataset
+dtool add item test-data demo-dataset
 
-dtool readme interactive 20211027-demo-dataset
+dtool readme interactive demo-dataset
 
-dtool add metadata 20211027-demo-dataset test-data purpose testing
+dtool add metadata demo-dataset test-data purpose testing
 
-dtool freeze 20211027-demo-dataset
+dtool freeze demo-dataset
 
-dtool overlays show 20211027-demo-dataset
+dtool overlays show demo-dataset
 
-dtool verify 20211027-demo-dataset
+dtool verify demo-dataset
 
-echo "addition" >> 20211027-demo-dataset/data/test-data
+echo "addition" >> demo-dataset/data/test-data
 
-dtool verify 20211027-demo-dataset
+dtool verify demo-dataset
 
---
+++ 030-dtool-lookup-server-instance/protocol.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+git clone https://github.com/jotelha/dtool-lookup-server-container-composition
+
+cd dtool-lookup-server-container-composition && bash tests/run.sh
