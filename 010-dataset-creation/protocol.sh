1a309ba07696b421ca5a48be5d23675b25d9228c
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2021-11-05 15:18:10 +0100

    Extended

--- 010-dataset-creation/protocol.sh
+++ 010-dataset-creation/protocol.sh
@@ -1,21 +1,31 @@
 #!/bin/bash -x
+# create README.yml template with simple metadata ontology
+cat dtool_readme_template.yml
+cp .dtool_readme.yml
+
+# create simple dtool.json configuration
+cat dtool.json
+mkdir -p ~/.config/dtool
+cp dtool.json ~/.config/dtool/
+
+# create and freeze dataset with one item
 dtool create demo-dataset
-
-echo test-content > test-data
-
-dtool add item test-data demo-dataset
-
+echo test-content > test-data-file.txt
+dtool add item test-data-file.txt demo-dataset
 dtool readme interactive demo-dataset
-
-dtool add metadata demo-dataset test-data purpose testing
-
 dtool freeze demo-dataset
 
-dtool overlays show demo-dataset
+# gather info
+dtool ls .
+dtool ls -v .
+
+dtool ls demo-dataset
+dtool ls -v demo-dataset
+
+dtool summary demo-dataset
+dtool summary -f json demo-dataset
 
 dtool verify demo-dataset
-
-echo "addition" >> demo-dataset/data/test-data
-
+echo "illegal-addition" >> demo-dataset/data/test-data-file.txt
 dtool verify demo-dataset
 
