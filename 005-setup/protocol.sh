8907b475a858fe3c675ee8350a572181026ca420
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2022-07-28 15:53:20 +0200

    ENH: setup only setup

--- 005-setup/protocol.sh
+++ 005-setup/protocol.sh
@@ -2,29 +2,7 @@
 python3 -m venv venv
 source venv/bin/activate
 
+pip install wheel
 pip install -r requirements.txt
 
 dtool --help
-
-# place dtool config in home folder
-cat dtool.json
-mkdir -p ~/.config/dtool
-cp dtool.json ~/.config/dtool/
-
-# inspect remote base URIs
-dtool ls s3://test-bucket
-dtool ls -v s3://test-bucket
-
-# inspect remote dataset
-dtool ls -v s3://test-bucket/1a1f9fad-8589-413e-9602-5bbd66bfe675
-dtool summary s3://test-bucket/1a1f9fad-8589-413e-9602-5bbd66bfe675
-
-# create local base URI
-mkdir -p datasets
-
-# copy dataset from remote to local
-dtool cp s3://test-bucket/1a1f9fad-8589-413e-9602-5bbd66bfe675 datasets
-
-# inspect local dataset
-dtool ls -v datasets
-dtool summary -h datasets/simple_test_dataset
